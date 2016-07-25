c3d58bae23b8868f802b963d44320e92447f0361
wtblife <wtblife@gmail.com>
2016-07-24 20:19:40 -0400

    Fixed coords file location.

--- sinatra.rb
+++ sinatra.rb
@@ -5,11 +5,11 @@ set :port, 3000
 
   get '/' do
   	@contents = []
-  	@contents.push File.read('../coords.txt')
+  	@contents.push File.read('../Configs/Coords.ini')
 	erb :map
   end
 
   get '/coords' do
-  	@coords = File.read('../coords.txt')
+  	@coords = File.read('../Configs/Coords.ini')
   	@coords
   end
